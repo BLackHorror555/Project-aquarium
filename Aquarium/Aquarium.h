d7d3a1c49ce933da6598215c2f4506027bc0ff8e
BlackHorror555 <mitronin1998@mail.ru>
2018-01-11 16:53:57 +0300

    1

---
+++ Aquarium/Aquarium.h
@@ -0,0 +1,15 @@
+#ifndef AQUARIM_H
+#define AQUARIUM_H
+#include <vector>
+class Aquarium
+{
+	int plancton_amount;
+	int fish_amount;
+	int shark_amount;
+
+public:
+	Aquarium();
+	~Aquarium();
+};
+
+#endif
